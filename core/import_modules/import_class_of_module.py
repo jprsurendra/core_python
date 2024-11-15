284c972fa56386936a9a45e601c8d973c0e120f9
surendra <jpr_surendra@yahoo.co.in>
2024-11-15 13:34:44 +0530

    import_module

---
+++ core/import_modules/import_class_of_module.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+# Now you can directly use the `datetime` class
+current_time = datetime.now()
+print(current_time)  # Output: 2024-11-15 13:32:26.893499
---
+++ core/import_modules/import_entire_module.py
@@ -0,0 +1,5 @@
+import datetime
+
+# Access the `datetime` class using `datetime.datetime`
+current_time = datetime.datetime.now()
+print(current_time)  # Output: 2024-11-15 13:34:16.132869
